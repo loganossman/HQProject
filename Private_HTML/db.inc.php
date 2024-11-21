1d42ca6d7411c74a13f51f2ce8fb75c8bf5ab6bb
Logan Ossman <loganossman@gmail.com>
2024-11-21 13:53:35 -0500

    Create db.inc.php

---
+++ Private_HTML/db.inc.php
@@ -0,0 +1,10 @@
+<?php
+try {
+    $pdo = new PDO("mysql:dbname=test;host=localhost", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+
+session_start();
+?>
